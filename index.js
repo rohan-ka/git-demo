35f97b2790029004607965a03e9dddfba1b2eebb
binoy <binoy.c@media.net>
2019-07-04 16:16:44 +0530

    add new feature

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ console.log('Important bugfix');
 
 console.log('new feature goes here');
 console.log('new feature enhanced');
+console.log('new feature enhanced again');
 console.log(' feature 1 enhanced');
 console.log(' feature 1 enhanced');
