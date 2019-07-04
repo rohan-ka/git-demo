b60c4a50a4e06dc67a45ebf9b0db022b44efa8e9
Rohan Kamat <rohan.ka@media.net>
2019-07-04 14:38:38 +0530

    add feature 3
    add feature 3

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ console.log('b');
 console.log('b');
 console.log('Important bugfix');
 console.log('Important bugfix');
+console.log('Important bugfix 5');
+console.log('feature 2');
+console.log('Important bugfix 3');
+console.log('Important bugfix 3');
 
 
 console.log('new feature goes here');
@@ -11,3 +15,6 @@ console.log('new feature enhanced');
 console.log('new feature enhanced again');
 console.log(' feature 1 enhanced');
 console.log(' feature 1 enhanced');
+console.log(' feature 3 enhanced');
+console.log(' feature 3 enhanced');
+console.log(' feature 5 enhanced');
